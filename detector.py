344666186422768ca7ebdf65aae3f045614dc231
Michael <michael.e@outlook.co.il>
2019-01-12 17:50:16 +0200

    pokemon

--- detector.py
+++ detector.py
@@ -5,7 +5,7 @@ from name_to_id import DataHandler
 # create image recognizer
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 # load the training data
-recognizer.read('analyzer/analyzer.yml')
+# recognizer.read('analyzer/analyzer.yml')
 # classifier path
 classifierPath = "Classifiers/face.xml"
 # new face classifier
@@ -29,15 +29,15 @@ while True:
     # for every axis in face
     for (x, y, w, h) in faces:
         # make a prediction base on analyzer data
-        predicted, conf = recognizer.predict(gray[y:y + h, x:x + w])
+        #predicted, conf = recognizer.predict(gray[y:y + h, x:x + w])
         # draw a rectangle based on offset
         cv2.rectangle(image, (x, y),
                       (x + w, y + h), BLUE, LINE_WIDTH)
         # convert prediction id to a name based on json
-        predicted = data_handler.get_name_by_id(predicted)
+        #predicted = data_handler.get_name_by_id(predicted)
         # put text on image
-        cv2.putText(image, str(predicted) + "--" + str(conf), (x, y + h), font, 1,
-                    RED, LINE_WIDTH)  # Draw the text
+        #cv2.putText(image, str(predicted) + "--" + str(conf), (x, y + h), font, 1,
+        #            RED, LINE_WIDTH)  # Draw the text
         # show the image
         cv2.imshow('Hackend\'s Face Recognition', image)
         cv2.waitKey(10)
